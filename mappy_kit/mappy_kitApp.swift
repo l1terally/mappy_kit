8f2a8dd21ff0ffb5e1f20587fb7ee0150faf40b2
l1terally <literallyuriy@gmail.com>
2023-01-05 18:26:19 +0800

    Initial Commit

---
+++ mappy_kit/mappy_kitApp.swift
@@ -0,0 +1,17 @@
+//
+//  mappy_kitApp.swift
+//  mappy_kit
+//
+//  Created by Юрий  Лизунов on 05/01/23.
+//
+
+import SwiftUI
+
+@main
+struct mappy_kitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
